cd3803bc457751070f74bb4550ae986906a69663
BoxEdition <2111221@my.ipleiria.pt>
2023-05-22 23:38:07 +0100

    Demo Keyboard

---
+++ src/components/CellComponent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div
+      :id="id"
+      :style="{ width: width + 'px', backgroundColor: color }"
+      class="cell"
+      draggable="true"
+      @dragstart="dragStart"
+    >
+      {{ title }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      id: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      width: {
+        type: Number,
+        required: true,
+      },
+      color: {
+        type: String,
+        required: true,
+      },
+      cellData: {
+        type: Object,
+        required: true,
+      },
+      onCellDrag: {
+        type: Function,
+        required: false,
+      },
+    },
+    methods: {
+      dragStart(event) {
+        event.dataTransfer.setData('text/plain', this.id);
+        this.onCellDrag(this.cellData);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .cell {
+    border: 1px solid #ccc;
+    padding: 10px;
+    cursor: move;
+  }
+  </style>
+  
---
+++ src/components/VirtualKeyboard.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="row">
+        <draggable :list="list" group="a" item-key="id" class="draggable-container">
+            <template #item="{ element }">
+                <CellComponent :id="element.id" :title="element.title" :width="element.width" :color="element.color"
+                    :cellData="element" :onCellDrag="handleCellDrag" />
+            </template>
+        </draggable>
+
+        <draggable :list="list2" group="a" item-key="id" class="draggable-container">
+            <template #item="{ element }">
+                <CellComponent :id="element.id" :title="element.title" :width="element.width" :color="element.color"
+                    :cellData="element" :onCellDrag="handleCellDrag" />
+            </template>
+        </draggable>
+        <draggable :list="list3" group="a" item-key="id" class="draggable-container">
+            <template #item="{ element }">
+                <CellComponent :id="element.id" :title="element.title" :width="element.width" :color="element.color"
+                    :cellData="element" :onCellDrag="handleCellDrag" />
+            </template>
+        </draggable>
+    </div>
+</template>
+  
+<script>
+import draggable from "vuedraggable";
+import CellComponent from "./CellComponent.vue";
+export default {
+    components: {
+        draggable,
+        CellComponent
+    },
+    data() {
+        return {
+            list: [
+                { id: "1", title: "Q", width: 40, color: "lightblue" },
+                { id: "2", title: "W", width: 40, color: "lightblue" },
+                { id: "3", title: "E", width: 40, color: "lightblue" },
+                { id: "4", title: "R", width: 40, color: "lightblue" },
+                { id: "5", title: "T", width: 40, color: "lightblue" },
+                { id: "6", title: "Y", width: 40, color: "lightblue" },
+                { id: "7", title: "U", width: 40, color: "lightblue" },
+                { id: "8", title: "I", width: 40, color: "lightblue" },
+                { id: "9", title: "O", width: 40, color: "lightblue" },
+                { id: "10", title: "P", width: 40, color: "lightblue" },
+            ],
+            list2: [
+                { id: "11", title: "A", width: 40, color: "lightgreen" },
+                { id: "12", title: "S", width: 40, color: "lightgreen" },
+                { id: "13", title: "D", width: 40, color: "lightgreen" },
+                { id: "14", title: "F", width: 40, color: "lightgreen" },
+                { id: "15", title: "G", width: 40, color: "lightgreen" },
+                { id: "16", title: "H", width: 40, color: "lightgreen" },
+                { id: "17", title: "J", width: 40, color: "lightgreen" },
+                { id: "18", title: "K", width: 40, color: "lightgreen" },
+                { id: "19", title: "L", width: 40, color: "lightgreen" },
+                { id: "20", title: "Ç", width: 40, color: "lightgreen" },
+
+            ],
+            list3: [
+                { id: "21", title: "Z", width: 40, color: "red" },
+                { id: "22", title: "X", width: 40, color: "red" },
+                { id: "23", title: "C", width: 40, color: "red" },
+                { id: "24", title: "V", width: 40, color: "red" },
+                { id: "25", title: "B", width: 40, color: "red" },
+                { id: "26", title: "N", width: 40, color: "red" },
+                { id: "27", title: "M", width: 40, color: "red" },
+                { id: "28", title: "K", width: 40, color: "red" },
+                { id: "29", title: ",", width: 40, color: "red" },
+                { id: "30", title: ".", width: 40, color: "red" }
+            ],
+        };
+
+    },
+    methods: {
+        handleCellDrag(cell) {
+            // Handle the cell drag event here
+            console.log('Cell dragged:', cell);
+        },
+    }
+};
+</script>
+<style scoped>
+.draggable-container {
+    display: flex;
+    flex-direction: row;
+}
+</style>
